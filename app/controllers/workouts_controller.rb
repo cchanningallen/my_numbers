f336213cc9a42fd9df8a98caeb274f4ddc45c937
Channing <cchanningallen@gmail.com>
2014-10-01 07:48:50 -0700

    add dataPointCategoryOptions constant to front + back

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,4 +3,6 @@ class WorkoutsController < ApplicationController
   expose(:workout)
 
   expose(:activity_options) { Workout::ACTIVITY_OPTIONS }
+
+  expose(:data_point_category_options) { ActivityType::DATA_POINT_CATEGORY_OPTIONS }
 end
